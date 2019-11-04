656aa008fe9aa5683c58bbb1c1d333d2d169bacd
Hazlenut <willie.wung@gmail.com>
2019-11-03 23:48:09 -0500

    making javascript for moving image

--- Scripts/move.js
+++ Scripts/move.js
@@ -1,4 +1,41 @@
-function Box(image, speed) {
+function Box(obj, container, image) {
+  this.$object = obj;
+  this.$container = container;
   this.image = image;
-  this.speed = speed;
+  this.speed = 100
+  this.position = {x: 0, y:0};
+}
+
+Box.prototype.setPosition = function() {
+  var x = Math.random(this.$container.innerWidth);
+  var y = Math.random(this.$container.innerHeight);
+  return {x: x, y: y};
+}
+
+Box.prototype.setImage = function(s) {
+  var element = document.createElement('../stylesheet.css');
+  element.style.content = s;
+}
+
+Box.prototype.setSpeed = function() {
+  this.speed = Math.random(200) + 100;
+}
+
+
+var = [1,2,3,4,5,6,7];
+var = ["redcircle.png", "orangecircle.png",
+"yellowcircle.png", "greencircle.png", "bluecircle.png",
+"purplecircle.png", "pinkcircle.png"];
+
+function shuffleArray(array) {
+  var currentI = array.length;
+  var tempV, randI;
+  while (0!= currentI) {
+    randI = Math.floor(Math.random() * currentI);
+    currentI-=1;
+    tempV = array[currentI];
+    array[currentI] = array[randI];
+    array[randI] = tempV;
+  }
+  return array;
 }
